51e35212b8c7b3a994b5424b21a058db3d66bced
MatheusAndradeeee <115434474+MatheusAndradeeee@users.noreply.github.com>
2024-04-22 18:41:25 -0400

    primeiro commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pds_atv
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
